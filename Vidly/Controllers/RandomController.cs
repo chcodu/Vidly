ab495207b5b44aea188cfa35f518a52db85dc7cb
Christopher <chcodu@gmail.com>
2019-09-28 12:22:11 -0600

    Segundo commit

---
+++ Vidly/Controllers/RandomController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class RandomController : Controller
+    {
+        // GET: Random
+        public ActionResult Index()
+        {
+            Movie movie = new Movie() { movie = "Sherk" };
+            return View(movie);
+        }
+    }
+}
